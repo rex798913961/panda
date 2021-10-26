aa110b62c9e3586336bf03d7b4483c86a884af5e
zhangzhifeng <798913961@qq.com>
2021-10-26 22:21:49 +0800

    feat(three):threejs提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,80 +6,81 @@
   </div>
 </template>
 
-<script>
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// <script>
+// import * as THREE from 'three'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-var scene, mesh;
-export default {
-  name: 'Home',
-  components: {
-  },
-  data () {
-    return {
-      camera: null,
-      renderer: null,
-      controls: null,
-      frequency: 0,
-      T0: 0
-    }
-  },
-  mounted () {
-    this.init()
-    this.animate()
-  },
-  methods: {
-    init () {
-      // 创建场景对象 scene
-      scene = new THREE.Scene()
-      let container = this.$refs.container
+// var scene, mesh;
+// export default {
+//   name: 'Home',
+//   components: {
+//   },
+//   data () {
+//     return {
+//       camera: null,
+//       renderer: null,
+//       controls: null,
+//       frequency: 0,
+//       T0: 0
+//     }
+//   },
+//   mounted () {
+//     this.init()
+//     this.animate()
+//   },
+//   methods: {
+//     init () {
+//       // 创建场景对象 scene
+//       scene = new THREE.Scene()
+//       let container = this.$refs.container
 
-      // 相机
-      this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 2000)
-      this.camera.position.z = 50
-      this.camera.position.y = 50;
+//       // 相机
+//       this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 2000)
+//       this.camera.position.z = 50
+//       this.camera.position.y = 50;
 
-      // 光源
-      const light = new THREE.DirectionalLight(0xffffff);
-      light.position.set(0.5, 1.0, 0.5).normalize();
-      scene.add(light);
+//       // 光源
+//       const light = new THREE.DirectionalLight(0xffffff);
+//       light.position.set(0.5, 1.0, 0.5).normalize();
+//       scene.add(light);
 
-      // Grid辅助线
-      const grid = new THREE.GridHelper(50, 50, 0xffffff, 0x555555);
-      scene.add(grid);
+//       // Grid辅助线
+//       const grid = new THREE.GridHelper(50, 50, 0xffffff, 0x555555);
+//       scene.add(grid);
 
-      // 创建网格模型 mesh
-      // 正方体BoxGeometry 
-      let geometry = new THREE.BoxGeometry(1, 1, 1)
-      // 材质对象 material
-      let material = new THREE.MeshNormalMaterial()
-      mesh = new THREE.Mesh(geometry, material)
-      mesh.position.set(0.5, 0.5, 0.5)
-      scene.add(mesh)
-      this.renderer = new THREE.WebGLRenderer({ antialias: true })
-      this.renderer.setSize(container.clientWidth, container.clientHeight)
-      this.renderer.setClearColor(0x999999, 1)
-      container.appendChild(this.renderer.domElement)
+//       // 创建网格模型 mesh
+//       // 正方体BoxGeometry 
+//       let geometry = new THREE.BoxGeometry(1, 1, 1)
+//       // 材质对象 material
+//       let material = new THREE.MeshNormalMaterial()
+//       mesh = new THREE.Mesh(geometry, material)
+//       mesh.position.set(0.5, 0.5, 0.5)
+//       scene.add(mesh)
+//       this.renderer = new THREE.WebGLRenderer({ antialias: true })
+//       this.renderer.setSize(container.clientWidth, container.clientHeight)
+//       this.renderer.setClearColor(0x999999, 1)
+//       container.appendChild(this.renderer.domElement)
 
-      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    },
-    animate () {
-      let T1 = new Date()
-      let t = T1 - this.T0
-      // console.log("频率",Math.ceil(1000/t))
-      this.frequency = Math.ceil(1000 / t)
-      this.T0 = T1
+//       this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+//     },
+//     animate () {
+//       let T1 = new Date()
+//       let t = T1 - this.T0
+//       // console.log("频率",Math.ceil(1000/t))
+//       this.frequency = Math.ceil(1000 / t)
+//       this.T0 = T1
 
-      requestAnimationFrame(this.animate)
-      // mesh.rotation.x += 0.01
-      // mesh.rotation.y += 0.02
-      //  mesh.rotateY(0.01);
-      this.controls.update();
-      this.renderer.render(scene, this.camera)
-    }
-  },
-}
-</script>
+//       requestAnimationFrame(this.animate)
+//       // mesh.rotation.x += 0.01
+//       // mesh.rotation.y += 0.02
+//       //  mesh.rotateY(0.01);
+//       this.controls.update();
+//       this.renderer.render(scene, this.camera)
+//     }
+//   },
+// }
+// </script>
+
 <style lang="less" scoped>
 .home {
   width: 100%;
